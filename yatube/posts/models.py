c0e5107c156bef416d840f82878fe5143c10e72a
Xostyara@gmail.com <Xostyara@gmail.com>
2022-07-30 01:27:46 +0300

    Спринт 3. Version 1.0.5. Исправления PEP 8

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,8 +6,8 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length = 200)
-    slug = models.SlugField(unique = True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
 
@@ -15,11 +15,11 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-        User, 
+        User,
         on_delete=models.CASCADE,
         related_name='posts')
     group = models.ForeignKey(
-        Group, 
-        blank=True, 
-        null=True, 
+        Group,
+        blank=True,
+        null=True,
         on_delete=models.CASCADE)
