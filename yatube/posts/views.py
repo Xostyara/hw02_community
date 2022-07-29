2a3222c8e98907f1baa7ef15346dd536f962d38b
Xostyara@gmail.com <Xostyara@gmail.com>
2022-07-29 23:08:20 +0300

    Спринт 3. Первый релиз

---
+++ yatube/posts/views.py
@@ -0,0 +1,37 @@
+from multiprocessing import context
+from re import template
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+# Create your views here.
+# Главная страница
+def index(request): 
+    template = 'posts/index.html'  
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        } 
+    return render(request, template, context)
+
+
+# Страница с постами группы
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+   # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    
+    return render(request, template, context)
+
+
